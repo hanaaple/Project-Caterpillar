34d8470dc6ec6d77238f8d82b968939a430fb1e1
UISU <seohsin1@naver.com>
2022-09-09 15:28:50 +0900

    Add Save System

--- Assets/Scripts/Utility/SaveSystem/SaveManager.cs
+++ Assets/Scripts/Utility/SaveSystem/SaveManager.cs
@@ -1,34 +1,93 @@
-using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
 using UnityEngine;
 
 namespace Utility.SaveSystem
 {
     public static class SaveManager
     {
-        private static string SaveFileName = $"{Application.persistentDataPath}/saveData.save";
+        private static int _idx;
+
+        private static string Savefilename => $"{Application.persistentDataPath}/saveData{_idx}.save";
+
         private static SaveData _saveData;
 
+        public static readonly byte[] EncryptKey = Encoding.UTF8.GetBytes("abcdefg_abcdefg_");
+        public static readonly byte[] EncryptIv = Encoding.UTF8.GetBytes("abcdefg_");
+        
+        
         public static void Init()
         {
+            _saveData = new SaveData();
 #if UNITY_IPHONE
         Environment.SetEnvironmentVariable("MONO_REFLECTION_SERIALIZER", "yes");
 #endif
         }
 
-        public static void Save()
+        public static SaveData GetSaveData()
         {
-            using var file = File.Create(SaveFileName);
-            new BinaryFormatter().Serialize(file, _saveData);
-
-            file.
-            
+            return _saveData;
         }
 
-        public static void Load()
+        public static void Save(int idx)
         {
+            _idx = idx;
+            RijndaelManaged rijn = new RijndaelManaged();
+            rijn.Mode = CipherMode.ECB;
+            rijn.Padding = PaddingMode.Zeros;
+            rijn.BlockSize = 256;
+            using (ICryptoTransform encryptor = rijn.CreateEncryptor(EncryptKey, EncryptIv))
+            {
+                using (var fileStream = File.Create(Savefilename))
+                {
+                    using (Stream cryptoStream = new CryptoStream(fileStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        new BinaryFormatter().Serialize(cryptoStream, _saveData);
+                    }
+                    fileStream.Close();
+                }
+            }
+            rijn.Clear();
+        }
 
+        public static bool Load(int idx)
+        {
+            Debug.Log(idx);
+            _idx = idx;
+            Debug.Log(Savefilename);
+            if (!File.Exists(Savefilename)) return false;
+            RijndaelManaged rijn = new RijndaelManaged();
+            rijn.Mode = CipherMode.ECB;
+            rijn.Padding = PaddingMode.Zeros;
+            rijn.BlockSize = 256;
+            
+            using (ICryptoTransform decryptor = rijn.CreateDecryptor(EncryptKey, EncryptIv))
+            {
+                using (var fileStream = File.Open(Savefilename, FileMode.Open))
+                {
+                    if (fileStream.Length <= 0)
+                    {
+                        return false;
+                    }
+                    using (Stream cryptoStream = new CryptoStream(fileStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        _saveData = (SaveData)new BinaryFormatter().Deserialize(cryptoStream);
+
+                    }
+                    fileStream.Close();
+                }
+            }
+
+            rijn.Clear();
+            // using var file = File.Open(Savefilename, FileMode.Open);
+            // if (file.Length > 0)
+            // {
+            //     _saveData = new BinaryFormatter().Deserialize(file) as SaveData;
+            // }
+
+            return true;
         }
     }
 }
