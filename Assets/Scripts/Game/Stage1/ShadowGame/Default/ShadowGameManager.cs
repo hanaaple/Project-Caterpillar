cae108ad98c6d4a1e8f9c39cc9584b0844196218
Uisu Seo <hanaaple@ajou.ac.kr>
2023-05-19 04:37:46 +0900

    Remove Unused using directive

--- Assets/Scripts/Game/Stage1/ShadowGame/Default/ShadowGameManager.cs
+++ Assets/Scripts/Game/Stage1/ShadowGame/Default/ShadowGameManager.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using Game.Default;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Utility.Core;
 using Utility.InputSystem;
